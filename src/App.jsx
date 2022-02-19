b0253de0f65eb13f4ba819631430710a399bbb1b
Marcos Vinicius <mvsouza7@gmail.com>
2022-02-19 10:53:38 +0200

    docs: the first main section of the webpage is created. nav, logo, buttons and a few images added and rendered in the page. the button component is also been used with styles variation and it is awesome! :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
-import GlobalStyles from "./components/styles/Global"
+import GlobalStyles from "./components/styles/Global";
 
 const theme = {
   colors: {
-    header: '#ebfbff',
-    body: '#fff',
-    footer: '#003333',
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
   },
-}
+};
 
 const App = () => {
   return (
-    <ThemeProvider theme = {theme}>
-    <>
-    <GlobalStyles />
-    <Header />
-    <Container>
-      <h1>Testing 1, 2, 3...</h1>
-    </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container></Container>
+      </>
     </ThemeProvider>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
-import { StyledHeader } from "./styles/Header.styled";
-import { Nav } from "./styles/Header.styled"
-import { Logo } from "./styles/Header.styled"
+import { StyledHeader, Nav, Logo, Image} from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
 import { Button } from "./styles/Button.styled";
-
-
+import { Flex } from "./styles/Flex.styled";
 
 function Header() {
   return (
     <StyledHeader>
       <Container>
         <Nav>
-          <Logo src="src\public\assets\logo.svg" alt="Logo"/>
-          <Button>Try it for free!</Button>
+          <Logo src="src\public\assets\logo.svg" alt="Logo" />
+          <Button>Try it free</Button>
         </Nav>
+
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engfage in genuine discussion.
+            </p>
+
+            <Button bg="#ff0099" color="#fff">
+              Get Started for Free
+            </Button>
+          </div>
+
+          <Image src="src\public\assets\illustration-mockups.svg" alt="mockups" />
+        </Flex>
       </Container>
     </StyledHeader>
   );
